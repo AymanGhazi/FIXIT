6d44c9d0d9ef4c915c31c9922815dcc106611546
AymanGhazi <aymanghazi2587@gmail.com>
2022-03-26 23:00:06 +0200

    first commit

---
+++ backend/src/main/java/edu/cauni/fixitapp/FixitappApplication.java
@@ -0,0 +1,13 @@
+package edu.cauni.fixitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixitappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixitappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/cauni/fixitapp/FixitappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cauni.fixitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixitappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
